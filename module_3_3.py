2c346c93118432c2c1440048ba31edf1b5d061e3
Роман <romangribanov@rambler.ru>
2024-08-19 13:36:30 +0300

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,19 @@
+# 1.Функция с параметрами по умолчанию:
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+
+
+print_params('string', 5, [3, 4, 2])
+print_params()
+print_params(b=25)
+print_params(c=[1, 2, 3])
+
+# 2.Распаковка параметров:
+values_list = [(9, 8, 7), 'СтРоКа', 32]
+values_dict = {'a': 'Any', 'b': [5, 4, 1], 'c': 15}
+print_params(*values_list)
+print_params(**values_dict)
+
+# 3.Распаковка + отдельные параметры:
+values_list_2 = [54.32, 'Строка']
+print_params(*values_list_2, 42)
